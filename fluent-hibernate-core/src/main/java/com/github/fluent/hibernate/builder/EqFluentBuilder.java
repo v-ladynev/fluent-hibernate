0b52a7154ae89014e034e0175ff75dea797c73ad
Lucas Levvy <mizyn24@gmail.com>
2015-10-08 22:01:51 +0200

    Disabled signing for example projects closes #15

--- fluent-hibernate-core/src/main/java/com/github/fluent/hibernate/builder/EqFluentBuilder.java
+++ fluent-hibernate-core/src/main/java/com/github/fluent/hibernate/builder/EqFluentBuilder.java
@@ -1,7 +1,6 @@
 package com.github.fluent.hibernate.builder;
 
-import org.hibernate.criterion.Criterion;
-import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.*;
 
 import com.github.fluent.hibernate.HibernateRequest;
 
@@ -10,38 +9,38 @@ import com.github.fluent.hibernate.HibernateRequest;
  */
 public class EqFluentBuilder implements IFluentBuilder {
 
-    private final String propertyName;
-    private final Object value;
+	private final String propertyName;
+	private final Object value;
 
-    //TODO взаимо исключаемые, добавить проверку
-    private boolean ifNotNull;
-    private boolean orIsNull;
+	// TODO there was smth but javadoc doesn't support this coding
+	private boolean ifNotNull;
+	private boolean orIsNull;
 
-    public EqFluentBuilder(String propertyName, Object value) {
-        this.propertyName = propertyName;
-        this.value = value;
-    }
+	public EqFluentBuilder(String propertyName, Object value) {
+		this.propertyName = propertyName;
+		this.value = value;
+	}
 
-    public IFluentBuilder ifNotNull() {
-        ifNotNull = true;
-        return this;
-    }
+	public IFluentBuilder ifNotNull() {
+		ifNotNull = true;
+		return this;
+	}
 
-    private Criterion getFalseRestriction() {
-        return Restrictions.sqlRestriction("1<>1");
-    }
+	private Criterion getFalseRestriction() {
+		return Restrictions.sqlRestriction("1<>1");
+	}
 
-    @Override
-    public <T> void build(HibernateRequest<T> hibernateRequest) {
-        if(orIsNull){
-            hibernateRequest.eqOrIsNull(propertyName, value);
-        } else if (!ifNotNull || value != null) {
-            hibernateRequest.eq(propertyName, value);
-        }
-    }
+	@Override
+	public <T> void build(HibernateRequest<T> hibernateRequest) {
+		if (orIsNull) {
+			hibernateRequest.eqOrIsNull(propertyName, value);
+		} else if (!ifNotNull || value != null) {
+			hibernateRequest.eq(propertyName, value);
+		}
+	}
 
-    public IFluentBuilder orIsNull() {
-        orIsNull = true;
-        return this;
-    }
+	public IFluentBuilder orIsNull() {
+		orIsNull = true;
+		return this;
+	}
 }
