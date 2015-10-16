ac5db8ae9b75d96bbce353eb50b3bf8348f4812c
v.ladynev <v.ladynev@gmail.com>
2015-10-16 12:32:26 -0300

    Add a new example project spring-console. It is just a template now.

---
+++ fluent-hibernate-examples/spring-console/src/main/java/com/github/fluent/hibernate/example/spring/console/SpringConsoleExample.java
@@ -0,0 +1,13 @@
+package com.github.fluent.hibernate.example.spring.console;
+
+/**
+ *
+ * @author V.Ladynev
+ */
+public class SpringConsoleExample {
+
+    public static void main(String[] args) {
+        System.out.println("xxx");
+    }
+
+}
