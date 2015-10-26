e5d9f89b1c3f57e671e3aa1c6a8e86920bc7df4f
v.ladynev <v.ladynev@gmail.com>
2015-10-26 03:42:40 -0300

    Add a Spring console example, closes #11

--- fluent-hibernate-examples/spring-console/src/main/java/com/github/fluent/hibernate/example/spring/console/SpringConsoleExample.java
+++ fluent-hibernate-examples/spring-console/src/main/java/com/github/fluent/hibernate/example/spring/console/SpringConsoleExample.java
@@ -3,9 +3,6 @@ package com.github.fluent.hibernate.example.spring.console;
 import java.util.Arrays;
 import java.util.List;
 
-import org.hibernate.SessionFactory;
-import org.jboss.logging.Logger;
-import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.github.fluent.hibernate.H;
@@ -20,38 +17,37 @@ import com.github.fluent.hibernate.factory.HibernateSessionFactory;
  */
 public class SpringConsoleExample {
 
-    private static final Logger LOG = Logger.getLogger(SpringConsoleExample.class);
-
     public static void main(String[] args) {
-        AbstractApplicationContext ctx = new ClassPathXmlApplicationContext(
-                "classpath:hibernate-context.xml");
-        // TODO check this with configuration errors
-        ctx.registerShutdownHook();
         try {
-            HibernateSessionFactory.Builder
-            .configureFromExistingSessionFactory((SessionFactory) ctx
-                    .getBean("sessionFactory"));
+            new ClassPathXmlApplicationContext("classpath:hibernate-context.xml")
+                    .registerShutdownHook();
             new SpringConsoleExample().doSomeDatabaseStuff();
         } finally {
-
             HibernateSessionFactory.closeSessionFactory();
         }
     }
 
     private void doSomeDatabaseStuff() {
         createTransactions();
+        loadTransactionsWithAllProperties();
+        loadTransactionsWithPartProperties();
+    }
+
+    private void loadTransactionsWithAllProperties() {
         List<Transaction> transactions = H.<Transaction> request(Transaction.class).list();
         System.out.println(transactions);
+    }
 
-        transactions = H.<Transaction> request(Transaction.class).innerJoin("customer")
-                .innerJoin("merchant").proj("customer.name").proj("merchant.name")
-                .proj("amountDue").transform(Transaction.class).list();
+    private void loadTransactionsWithPartProperties() {
+        List<Transaction> transactions = H.<Transaction> request(Transaction.class)
+                .innerJoin("customer").innerJoin("merchant").proj("customer.name")
+                .proj("merchant.name").proj("amountDue").transform(Transaction.class).list();
         System.out.println(transactions);
     }
 
     private void createTransactions() {
-        Customer customer = save(Customer.create("Jhon"));
-        Merchant merchant = save(Merchant.create("Doe"));
+        Customer customer = H.save(Customer.create("Jhon"));
+        Merchant merchant = H.save(Merchant.create("Doe"));
         Transaction t1000 = Transaction.create(customer, merchant);
         t1000.setAmountDue(1000L);
 
@@ -59,11 +55,6 @@ public class SpringConsoleExample {
         t2000.setAmountDue(2000L);
 
         H.saveAll(Arrays.asList(t1000, t2000));
-    }
-
-    private static <T> T save(T persistent) {
-        H.save(persistent);
-        return persistent;
     }
 
 }
